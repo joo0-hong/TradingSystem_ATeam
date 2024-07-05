898518b74c66421103f6f61a1b74c8e316b9515c
taeung.yoon <ytw0201@gmail.com>
2024-07-05 15:39:28 +0900

    [feature] modify getPrice method

--- TradingSystem/StockerBrockerDriver.h
+++ TradingSystem/StockerBrockerDriver.h
@@ -7,5 +7,5 @@ interface StockerBrockerDriver
 	virtual void logIn(const std::string& id, const std::string& pass) = 0;
 	virtual void buyStock(const std::string& stock, const int price, const int amount) = 0;
 	virtual void sellStock(const std::string& stock, const int price, const int amount) = 0;
-	virtual int getPrice(const std::string& stock);
+	virtual int getPrice(const std::string& stock) = 0;
 };
