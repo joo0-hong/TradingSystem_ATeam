65ada6c07d5423fe3a4c0f97434459b6c5a67909
hack.jang <hack.jang@samsung.com>
2024-07-05 15:00:02 +0900

    feature app logic exception

--- TradingSystem/JusikApplication.cpp
+++ TradingSystem/JusikApplication.cpp
@@ -1,8 +1,11 @@
 #include "JusikApplication.h"
 
-JusikApplication::JusikApplication()
-{
+class LoginFailException : public std::exception {};
+class ZeroPriceTradingException : public std::exception {};
 
+JusikApplication::JusikApplication()
+	: isLogedIn_{ false }
+{
 }
 
 void JusikApplication::selectStockBrocker(const std::string& stockBrocker)
@@ -12,20 +15,37 @@ void JusikApplication::selectStockBrocker(const std::string& stockBrocker)
 
 void JusikApplication::login(const std::string& id, const std::string& pass)
 {
-
+	isLogedIn_ = true;
 }
 
 int JusikApplication::getPrice(const std::string& stock)
 {
-	return 0;
+	return 1;
 }
 
 void JusikApplication::buy(const std::string& stock, const int price)
 {
-
+	checkCanBuyState(price);
+}
+void JusikApplication::checkCanBuyState(const int price)
+{
+	throwExceptionWhenNotLoggedIn();
+	throwExceptionWhenZeroPriceTrade(price);
+}
+void JusikApplication::throwExceptionWhenNotLoggedIn()
+{
+	if (isLogedIn_ == false) {
+		throw LoginFailException();
+	}
+}
+void JusikApplication::throwExceptionWhenZeroPriceTrade(const int price)
+{
+	if (price <= 0) {
+		throw ZeroPriceTradingException();
+	}
 }
 
 void JusikApplication::sell(const std::string& stock, const int price)
 {
-
+	throwExceptionWhenZeroPriceTrade(price);
 }
