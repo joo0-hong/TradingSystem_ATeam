7930e049c78a113105467e87b48e78da244b6ca4
taeung.yoon <ytw0201@gmail.com>
2024-07-05 14:33:20 +0900

    [feagure] Implement default API for Jusik Application

--- TradingSystem/JusikApplication.cpp
+++ TradingSystem/JusikApplication.cpp
@@ -4,3 +4,28 @@ JusikApplication::JusikApplication()
 {
 
 }
+
+void JusikApplication::selectStockBrocker(const std::string& stockBrocker)
+{
+
+}
+
+void JusikApplication::login(const std::string& id, const std::string& pass)
+{
+
+}
+
+int JusikApplication::getPrice(const std::string& stock)
+{
+	return 0;
+}
+
+void JusikApplication::buy(const std::string& stock, const int price)
+{
+
+}
+
+void JusikApplication::sell(const std::string& stock, const int price)
+{
+
+}
--- TradingSystem/JusikApplication.h
+++ TradingSystem/JusikApplication.h
@@ -1,7 +1,14 @@
 #pragma once
+#include <string>
 
 class JusikApplication
 {
 public:
     JusikApplication();
+
+    void selectStockBrocker(const std::string& stockBrocker);
+    void login(const std::string& id, const std::string& pass);
+    int getPrice(const std::string& stock);
+    void buy(const std::string& stock, const int price);
+    void sell(const std::string& stock, const int price);
 };
