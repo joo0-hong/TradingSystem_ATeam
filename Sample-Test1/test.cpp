61ff533f1553804818f5da949af9c9425c7aebcf
jooyoung <hjoo820@naver.com>
2024-07-05 14:45:33 +0900

    Invalid parameter tests

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -5,21 +5,53 @@
 using namespace std;
 using namespace testing;
 
-TEST(ApplicationTest, StartApplication) {
+class AppFixture : public testing::Test {
+public:
 	JusikApplication app;
+};
 
-	EXPECT_THAT(&app, NotNull());
+TEST(ApplicationTest, StartApplication) {
+	JusikApplication* appPointer = new JusikApplication();
+	EXPECT_THAT(appPointer, NotNull());
 }
 
-TEST(ApplicationTest, AppDefaultAPI) {
+TEST_F(AppFixture, AppDefaultAPI) {
 	JusikApplication app;
 
 	app.selectStockBrocker("Kiwer");
 	app.login("ATeam", "1234qwer");
 
 	int currentPrice = app.getPrice("Samsung");
-	EXPECT_THAT(currentPrice, Ge(0));
+	EXPECT_THAT(currentPrice, Gt(0));
 
 	app.buy("Samsung", 80000);
 	app.sell("Samsung", 100000);
 }
+
+TEST_F(AppFixture, BuyingWithoutLogin) {
+	app.selectStockBrocker("Nemo");
+
+	EXPECT_THROW({
+		app.buy("Samsung", 80000);
+		}, std::exception);
+}
+
+TEST_F(AppFixture, ZeroBuying) {
+	app.selectStockBrocker("Nemo");
+	app.login("ATeam", "1234qwer");
+
+	EXPECT_THROW({
+		app.buy("Samsung", 0);
+		}, std::exception);
+}
+
+TEST_F(AppFixture, ZeroSelling) {
+	app.selectStockBrocker("Nemo");
+	app.login("ATeam", "1234qwer");
+
+	EXPECT_THROW({
+		app.sell("Samsung", 0);
+		}, std::exception);
+}
+
+
