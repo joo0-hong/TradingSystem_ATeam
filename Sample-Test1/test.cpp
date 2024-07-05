9a1dcc84d6b68ed43ab50d01517e5644c246e21a
jooyoung <hjoo820@naver.com>
2024-07-05 16:35:00 +0900

    Added nice timing tests

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -25,6 +25,13 @@ public:
 		app.login("ATeam", "1234qwer");
 	}
 
+	const int NICE_TIMING_CHECK_COUNT = 3;
+	const int MY_BUDGET = 500000;
+	const string STOCK_BROCKER_NEMO = "Nemo";
+	const string STOCK_BROCKER_KIWER = "Kiwer";
+	const string STOCK_SAMPLE_1 = "Samsung";
+	const string STOCK_SAMPLE_2 = "Hynix";
+
 	NiceMock<BrockerMock> mk;
 	JusikApplication app;
 };
@@ -39,37 +46,37 @@ TEST(ApplicationTest, StartApplication) {
 TEST_F(AppFixture, AppDefaultAPI) {
 	JusikApplication app;
 
-	app.selectStockBrocker("Kiwer");
+	app.selectStockBrocker(STOCK_BROCKER_KIWER);
 	app.login("ATeam", "1234qwer");
 
-	int currentPrice = app.getPrice("Samsung");
+	int currentPrice = app.getPrice(STOCK_SAMPLE_1);
 	EXPECT_THAT(currentPrice, Gt(0));
 
-	app.buy("Samsung", 80000, 1);
-	app.sell("Samsung", 100000, 1);
+	app.buy(STOCK_SAMPLE_1, 80000, 1);
+	app.sell(STOCK_SAMPLE_1, 100000, 1);
 }
 
 TEST_F(AppFixture, BuyingWithoutLogin) {
-	app.selectStockBrocker("Nemo");
+	app.selectStockBrocker(STOCK_BROCKER_NEMO);
 
 	EXPECT_THROW({
-		app.buy("Samsung", 80000, 1);
+		app.buy(STOCK_SAMPLE_1, 80000, 1);
 		}, std::exception);
 }
 
 TEST_F(AppFixture, ZeroPriceBuying) {
-	login("Nemo");
+	login(STOCK_BROCKER_NEMO);
 
 	EXPECT_THROW({
-		app.buy("Samsung", 0, 1);
+		app.buy(STOCK_SAMPLE_1, 0, 1);
 		}, std::exception);
 }
 
 TEST_F(AppFixture, ZeroPriceSelling) {
-	login("Nemo");
+	login(STOCK_BROCKER_NEMO);
 
 	EXPECT_THROW({
-		app.sell("Samsung", 0, 1);
+		app.sell(STOCK_SAMPLE_1, 0, 1);
 		}, std::exception);
 }
 
@@ -78,5 +85,53 @@ TEST_F(AppFixture, APIsCallingLogin) {
 	EXPECT_CALL(mk, logIn(_, _))
 		.Times(1);
 
-	login("Nemo");
+	login(STOCK_BROCKER_NEMO);
+}
+
+TEST_F(AppFixture, buyNiceTiming) {
+	login(STOCK_BROCKER_NEMO);
+
+	EXPECT_CALL(mk, getPrice(_))
+		.Times(NICE_TIMING_CHECK_COUNT);
+
+	app.buyNiceTiming(STOCK_SAMPLE_2, MY_BUDGET);
+}
+
+TEST_F(AppFixture, buyNiceTimingIncreaseStatus) {
+	login(STOCK_BROCKER_NEMO);
+
+	EXPECT_CALL(mk, getPrice(STOCK_SAMPLE_2))
+		.Times(NICE_TIMING_CHECK_COUNT)
+		.WillOnce(Return(40000))
+		.WillOnce(Return(50000))
+		.WillOnce(Return(60000));
+
+	EXPECT_CALL(mk, buyStock(STOCK_SAMPLE_2, _, _))
+		.Times(AtLeast(1));
+
+	app.buyNiceTiming(STOCK_SAMPLE_2, MY_BUDGET);
+}
+
+TEST_F(AppFixture, SellNiceTiming) {
+	login(STOCK_BROCKER_NEMO);
+
+	EXPECT_CALL(mk, getPrice(_))
+		.Times(NICE_TIMING_CHECK_COUNT);
+
+	app.sellNiceTiming(STOCK_SAMPLE_2, 1);
+}
+
+TEST_F(AppFixture, buyNiceTimingIncreaseStatus) {
+	login(STOCK_BROCKER_NEMO);
+
+	EXPECT_CALL(mk, getPrice(STOCK_SAMPLE_2))
+		.Times(NICE_TIMING_CHECK_COUNT)
+		.WillOnce(Return(60000))
+		.WillOnce(Return(50000))
+		.WillOnce(Return(40000));
+
+	EXPECT_CALL(mk, sellStock(STOCK_SAMPLE_2, _, _))
+		.Times(AtLeast(1));
+
+	app.sellNiceTiming(STOCK_SAMPLE_2, 1);
 }
